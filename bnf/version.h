79512e1af7d33b0f938b04bde366292d115d6874
Joey Pabalinas <joeypabalinas@gmail.com>
2018-10-28 10:00:17 -1000

    bnf: add parser generator

---
+++ bnf/version.h
@@ -0,0 +1,14 @@
+/*
+ * version.h - package version infos
+ */
+
+#ifndef INCLUDED_VERSION_H
+#define INCLUDED_VERSION_H
+
+#include "config.h"
+
+#define PKG_NAME    PACKAGE
+#define PKG_VERSION VERSION
+
+#endif /* INCLUDED_VERSION_H */
+
